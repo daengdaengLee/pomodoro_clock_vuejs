595242dea5b8dc0fbc7bc69e548b5d6bb6f52ff7
KunhoLee <gunho1020@gmail.com>
2017-12-10 23:27:59 +0900

    setter 컴포넌트 설계중

---
+++ src/components/Setter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Setter">
+    <div class="NumDisplay">{{ displayNum }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Setter',
+  data () {
+    return {
+      displayNum: 5
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
