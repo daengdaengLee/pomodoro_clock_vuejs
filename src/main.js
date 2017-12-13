182de2a74731b8cafdb19b701ada8df32e158393
KunhoLee <gunho1020@gmail.com>
2017-12-13 12:46:19 +0900

    es6-promise polyfill 적용

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ES6Promise from 'es6-promise'
 import App from './App'
 import store from './store'
+
+ES6Promise.polyfill()
 
 Vue.config.productionTip = false
 
