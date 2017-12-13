352e3fb01528c78179933cfedc9a304562bcb510
KunhoLee <gunho1020@gmail.com>
2017-12-13 12:58:00 +0900

    Display component에서 timer 로직 작성

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -4,7 +4,7 @@
       {{ state }}
     </div>
     <div class="Timer">
-      {{ timer }}, {{ timerOnOff }}
+      {{ timer }}, {{ timerOnOff }}, {{ timerId }}
     </div>
   </div>
 </template>
@@ -19,14 +19,23 @@ export default {
     return {
       state: 'Session',
       timerCount: 0,
-      timerOnOff: false
+      timerOnOff: false,
+      timerId: null
     }
   },
   computed: _.extend(
     {
+      totalSessionSecs () {
+        return this.sessionLength * 60
+      },
+      totalBreakSecs () {
+        return this.breakLength * 60
+      },
       timer () {
         if (this.state === 'Session') {
-          return this.sessionLength - this.timerCount
+          return this.calculateTimer(this.totalSessionSecs, this.timerCount)
+        } else if (this.state === 'Break') {
+          return this.calculateTimer(this.totalBreakSecs, this.timerCount)
         }
       }
     },
@@ -35,6 +44,31 @@ export default {
   methods: {
     timerClick () {
       this.timerOnOff = !this.timerOnOff
+      if (this.timerOnOff) {
+      // 타이머 작동
+        this.timerId = setInterval(() => {
+          this.timerCount += 1
+        }, 1000)
+      } else {
+      // 타이머 중지
+        clearInterval(this.timerId)
+        this.timerId = null
+      }
+    },
+    calculateTimer (total, count) {
+      const remainTotalSecs = total - count
+      const remainMin = Math.floor(remainTotalSecs / 60)
+      const remainSecs = remainTotalSecs - (remainMin * 60)
+      const remainMinString = String(remainMin)
+      let remainSecsString
+      switch (remainSecs) {
+        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8: case 9:
+          remainSecsString = '0' + String(remainSecs)
+          break
+        default:
+          remainSecsString = String(remainSecs)
+      }
+      return `${remainMinString} : ${remainSecsString}`
     }
   }
 }
